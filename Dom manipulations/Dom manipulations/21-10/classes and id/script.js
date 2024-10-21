acf091b6e053d1d0bed230cb388ce715163f8c5d
salman-itShaala <salman.shaikh@itshaala.com>
2024-10-21 15:02:35 +0530

    added class related concepts

---
+++ Dom manipulations/Dom manipulations/21-10/classes and id/script.js
@@ -0,0 +1,19 @@
+const container = document.getElementById("container");
+
+// to get the classes
+console.log(container.classList);
+
+// to add another class
+container.classList.add("box-1");
+
+// to remove the class
+container.classList.remove("box");
+
+// to check if class is present or not
+container.classList.contains("active");
+
+function showOptions() {
+  // to toggle between classes
+
+  container.classList.toggle("active");
+}
